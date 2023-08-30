110c4d6b562f7bba1131df00b55b4abe5b203075
WeizmannBD <weizmann.bd@gmail.com>
2023-08-30 16:09:15 +0300

    Rename file (typo) and fixed batch file

--- services/VacuumLogger/Run_Vacuum_Logger.bat
+++ services/VacuumLogger/Run_Vacuum_Logger.bat
@@ -1,6 +1,7 @@
 @ECHO ON
 setlocal
-set PYTHONPATH="C:\Users\Lab112\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Lab112\AppData\Local\Programs\Python\Python310\Lib;C:\Users\Lab112\AppData\Local\Programs\Python\Python310;C:\Users\Lab112\PycharmProjects\NewOPXplus\venv;C:\Users\Lab112\PycharmProjects\NewOPXplus\venv\Lib\site-packages;C:\Users\Lab112\PycharmProjects\NewOPXplus\site-packages\python_vxi11-0.9-py3.9.egg;C:\Users\Lab112\PycharmProjects\GUI-Experiment\GUI-Experiment;C:\Users\Lab112\PycharmProjects\GUI-Experiment\GUI-Experiment\widgets\scopeWidget"
-python C:\Users\Lab112\PycharmProjects\GUI-Experiment\GUI-Experiment\services\VacuumLogger\VacuumLogger.py
+set PYTHONPATH="C:\Users\Lab112\PycharmProjects\GUI-Experiment\functions;C:\Users\Lab112\PycharmProjects\GUI-Experiment\services;C:\Users\Lab112\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Lab112\AppData\Local\Programs\Python\Python310\Lib;C:\Users\Lab112\AppData\Local\Programs\Python\Python310;C:\Users\Lab112\PycharmProjects\NewOPXplus\venv;C:\Users\Lab112\PycharmProjects\NewOPXplus\venv\Lib\site-packages;C:\Users\Lab112\PycharmProjects\NewOPXplus\site-packages\python_vxi11-0.9-py3.9.egg;C:\Users\Lab112\PycharmProjects\GUI-Experiment;C:\Users\Lab112\PycharmProjects\GUI-Experiment\GUI-Experiment\widgets\scopeWidget"
+python --version
+python C:\Users\Lab112\PycharmProjects\GUI-Experiment\services\VacuumLogger\VacuumLogger.py
 endlocal
 pause
