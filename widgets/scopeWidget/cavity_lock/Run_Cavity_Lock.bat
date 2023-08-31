9b48211d48ac3319be9f684f0c9bcfe920dad7a7
WeizmannBD <weizmann.bd@gmail.com>
2023-08-31 11:51:10 +0300

    Fix batch file for Cavity Lock

--- widgets/scopeWidget/cavity_lock/Run_Cavity_Lock.bat
+++ widgets/scopeWidget/cavity_lock/Run_Cavity_Lock.bat
@@ -1,6 +1,6 @@
 @ECHO ON
 setlocal
-set PYTHONPATH="C:\Users\Lab112\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Lab112\AppData\Local\Programs\Python\Python310\Lib;C:\Users\Lab112\AppData\Local\Programs\Python\Python310;C:\Users\Lab112\PycharmProjects\NewOPXplus\venv;C:\Users\Lab112\PycharmProjects\NewOPXplus\venv\Lib\site-packages;C:\Users\Lab112\PycharmProjects\NewOPXplus\site-packages\python_vxi11-0.9-py3.9.egg;C:\Users\Lab112\PycharmProjects\GUI-Experiment\GUI-Experiment;C:\Users\Lab112\PycharmProjects\GUI-Experiment\GUI-Experiment\widgets\scopeWidget"
-python C:\Users\Lab112\PycharmProjects\GUI-Experiment\GUI-Experiment\widgets\scopeWidget\cavity_lock\cavity_lock.py
+set PYTHONPATH="C:\Users\Lab112\PycharmProjects\GUI-Experiment;C:\Users\Lab112\PycharmProjects\GUI-Experiment\services;C:\Users\Lab112\PycharmProjects\GUI-Experiment\functions;C:\Users\Lab112\AppData\Local\Programs\Python\Python310\DLLs;C:\Users\Lab112\AppData\Local\Programs\Python\Python310\Lib;C:\Users\Lab112\AppData\Local\Programs\Python\Python310;C:\Users\Lab112\PycharmProjects\NewOPXplus\venv;C:\Users\Lab112\PycharmProjects\NewOPXplus\venv\Lib\site-packages;C:\Users\Lab112\PycharmProjects\NewOPXplus\site-packages\python_vxi11-0.9-py3.9.egg;C:\Users\Lab112\PycharmProjects\GUI-Experiment;C:\Users\Lab112\PycharmProjects\GUI-Experiment\widgets\scopeWidget"
+python C:\Users\Lab112\PycharmProjects\GUI-Experiment\widgets\scopeWidget\cavity_lock\cavity_lock.py
 endlocal
 pause
