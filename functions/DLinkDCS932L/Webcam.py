e8083c3920ce1f3246cab2b5813b317f4ba24acd
Dror <drorg@wismain.weizmann.ac.il>
2023-11-21 14:49:42 +0200

    (1) Final touches to Lorentzian fit. (2) Add calibration capability to webcam (using arrows) (3) Checkin more test-cases for lorentzian fit

--- functions/DLinkDCS932L/Webcam.py
+++ functions/DLinkDCS932L/Webcam.py
@@ -8,7 +8,9 @@ import cv2
 import numpy as np
 import keyboard
 import time
-
+from matplotlib import pyplot as plt
+#from matplotlib import image as mpimg
+import sys
 
 # -----------------------------------------------------------
 # Important Notes:
@@ -34,13 +36,13 @@ class Webcam:
         pytesseract.pytesseract.tesseract_cmd = tesseract_exe_path
         pass
 
-    def get_image(self):
+    def get_image(self, crop=True):
         response = requests.get(self.url)
         img = Image.open(BytesIO(response.content))
         nimg = np.asarray(img)
 
         # Need cropping?
-        if self.x is not None:
+        if self.x is not None and crop:
             nimg = nimg[self.y:self.y + self.h, self.x:self.x + self.w]
 
         return nimg
@@ -55,9 +57,22 @@ class Webcam:
         self.text = None
         if mode == 'rgb':
             self.text = self._ocr_rgb(debug=debug)
-        else:
+        elif mode == 'grey':
             self.text = self._ocr_greyscale(debug=debug)
+        else:
+            self.text = self._ocr_experimental(debug=debug)
         return self.text
+
+    def _ocr_experimental(self, debug=False):
+
+        image = self.get_image()
+        gry = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Look at a given row
+        row = image[0]
+        #img_array = np.array(image, dtype=np.uint8)
+
+
 
     def _ocr_rgb(self, psm=7, debug=False):
         rgb = self.get_image()
@@ -84,26 +99,69 @@ class Webcam:
 
         return text
 
+    def on_press(self, event):
+        #print('press', event.key)
+        sys.stdout.flush()
+        should_update = True
+        weight = 1
+
+        if event.key.startswith('shift+'):
+            weight = 10
+            event.key = event.key.replace('shift+', '')
+
+        if event.key == 'x':
+            self.frame = cv2.bitwise_not(self.frame)
+            self.im.set_data(self.frame)
+            self.fig.canvas.draw()
+            should_update = False
+        elif event.key == 'left':
+            self.x = self.x - weight
+        elif event.key == 'right':
+            self.x = self.x + weight
+        elif event.key == 'up':
+            self.y = self.y - weight
+        elif event.key == 'down':
+            self.y = self.y + weight
+        elif event.key == '[':
+            self.w = self.w + weight
+        elif event.key == ']':
+            self.w = self.w - weight
+        elif event.key == 'a':
+            self.h = self.h + 1
+        elif event.key == 'A':
+            self.h = self.h + 10
+        elif event.key == 'z':
+            self.h = self.h - 1
+        elif event.key == 'Z':
+            self.h = self.h - 10
+        else:
+            should_update = False
+
+        if should_update:
+            self.frame = self.org_frame[self.y:self.y + self.h, self.x:self.x + self.w]
+            self.im.set_data(self.frame)
+            self.fig.canvas.draw()
+            print(f'x={self.x}, y={self.y}, h={self.h}, w={self.w}')
+
     def calibrate(self):
-        import cv2
+        """
+        This method brings the camera image and allows to move it left/right (use arrows) or change the width
+        (with '[' ot ']') or change the height (with 'a' or 'z')
+        """
 
-        while True:
+        self.org_frame = self.get_image(crop=False)
 
-            if keyboard.is_pressed('q'):  # if key 'q' is pressed
-                print('You Pressed A Key!')
-                break  # finishing the loop
+        self.frame = self.org_frame[self.y:self.y + self.h, self.x:self.x + self.w]
 
-            frame = self.get_image()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+        plt.title("Position: Up-arrow Down-arrow  Width: [ ]  Height: a z")
+        #plt.xlabel("X pixel scaling")
+        #plt.ylabel("Y pixels scaling")
 
-            cv2.imshow("Image", frame)
+        self.fig, ax = plt.subplots()
+        self.fig.canvas.mpl_connect('key_press_event', self.on_press)
 
-            time.sleep(1)
-
-            cv2.destroyWindow("Image")
-
-            pass
+        self.im = plt.imshow(self.frame)
+        plt.show()
 
         pass
 
