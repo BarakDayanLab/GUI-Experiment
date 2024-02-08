de1b7fe277bde717cd80863634057ac4d552c9c8
WeizmannBD <weizmann.bd@gmail.com>
2024-02-08 19:53:28 +0200

    Add some IPs to config file

--- services/new_cavity_lock/config/general.py
+++ services/new_cavity_lock/config/general.py
@@ -1,8 +1,11 @@
 
 # ---------------- MAIN EXP SOCKET ---------------- #
-SOCKET_IP = "132.77.55.172"
+SOCKET_IP_114 = "132.77.55.134"
+SOCKET_IP_113 = "132.77.55.172"
+SOCKET_IP_112 = "132.77.54.212"
+SOCKET_IP = "132.77.54.212"
 SOCKET_PORT = 5050
-SEND_DATA_INTERVAL = 3
+SEND_DATA_INTERVAL = 1
 
 # ----------------- SAVE ----------------- #
 FOLDER_PATH = "C:\\Users\\nadavkan.WISMAIN\\PycharmProjects\\GUI-Experiment\\services\\new_cavity_lock\\data"
